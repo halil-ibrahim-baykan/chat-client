bb5dfcdfb4eb60a1bc55456eb4239282471df057
halil-ibrahim-baykan <halilibrahimbaykan@gmail.com>
2019-11-15 13:32:58 +0100

    stream resrt submit add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import superagent from "superagent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    messages: [],
+    value: ""
+  };
+
+  stream = new EventSource("http://localhost:4000/stream");
+
+  componentDidMount() {
+    this.stream.onmessage = event => {
+      const { data } = event;
+      console.log("data test", data);
+
+      const parsedData = JSON.parse(data);
+      if (Array.isArray(parsedData)) {
+        console.log("array test");
+
+        this.setState({ messages: parsedData });
+      } else {
+        console.log("else test");
+
+        const messages = [...this.state.messages, parsedData];
+        this.setState({ messages });
+      }
+    };
+  }
+
+  onChange = event => {
+    const { value } = event.target;
+    console.log("VALUE TEST", value);
+    this.setState({ value });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+
+    const { value } = this.state;
+    const url = "http://localhost:4000/message";
+
+    superagent
+      .post(url)
+      .send({ message: value })
+      .then(response => {
+        console.log("response test", response);
+      });
+  };
+
+  reset = () => {
+    this.setState({ value: "" });
+  };
+
+  render() {
+    const list = this.state.messages.map((message, index) => (
+      <p key={index}>{message}</p>
+    ));
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.onChange}
+          />
+          <button type="button" onClick={this.reset}>
+            Reset
+          </button>
+          <button>Submit</button>
+        </form>
+
+        {list}
+      </div>
+    );
+  }
 }
-
-export default App;
